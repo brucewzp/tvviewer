44fef5c2c8d4d6157498460b545bc29724fc36b4
Bruce <brucewzp@gmail.com>
2016-06-20 22:26:46 +0800

    initial player

--- tvviewer/ViewController.swift
+++ tvviewer/ViewController.swift
@@ -7,13 +7,28 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
+    var moviePlayer : MPMoviePlayerController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        let path = "http://61.189.255.152/live.aishang.ctlcdn.com/00000110240127_1/encoder/0/playlist.m3u8"
+        let url = NSURL(string:path)
+        self.moviePlayer = MPMoviePlayerController(contentURL: url)
+        if let player = self.moviePlayer {
+            player.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+            player.view.sizeToFit()
+            player.scalingMode = MPMovieScalingMode.Fill
+            player.fullscreen = true
+            player.controlStyle = MPMovieControlStyle.None
+            player.movieSourceType = MPMovieSourceType.File
+            player.repeatMode = MPMovieRepeatMode.One
+            player.play()
+            self.view.addSubview(player.view)
+        }}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
